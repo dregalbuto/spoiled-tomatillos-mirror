079fda7a0b95e323743f1655255eec1a0261dbda
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-02-07 16:09:48 -0500

    TEAM24-10: Fixed mapping error

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/HelloObject.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/HelloObject.java
@@ -5,31 +5,48 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name="hello")
+/**
+ * Simple example of CRU services as an object
+ */
+@Entity(name="test")
 public class HelloObject {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	private String message;
-	
-	public HelloObject(String message) {
-		this.message = message;
-	}
-	
 	public int getId() {
-		return this.id;
+		return id;
 	}
-	
 	public void setId(int id) {
 		this.id = id;
 	}
 	
-	public String getMessage() {
-		return message;
+	private String message;
+	
+	/**
+	 * Default constructor that gives the object an empty string
+	 */
+	public HelloObject() {
+		this.message = new String();
 	}
 	
+	/** Constructor with initial message
+	 * @param message: The message this object should contain
+	 */
+	public HelloObject(String message) {
+		this.message = message;
+	}
+	
+	/** Setter for Hello World POJO
+	 * @param message: The message this object should contain
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
 	
+	/** Getter for Hello World POJO
+	 * @return The message this object contains
+	 */
+	public String getMessage() {
+		return message;
+	}
 }
