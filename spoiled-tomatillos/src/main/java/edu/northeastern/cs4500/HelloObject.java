b0af2173d1924b335c696b5c12f61502223d5696
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-02-07 15:23:08 -0500

    MySQL integration

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/HelloObject.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/HelloObject.java
@@ -1,10 +1,27 @@
 package edu.northeastern.cs4500;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="hello")
 public class HelloObject {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 	private String message;
 	
 	public HelloObject(String message) {
 		this.message = message;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 	public String getMessage() {
--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/SpoiledTomatillosApplication.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/SpoiledTomatillosApplication.java
@@ -3,6 +3,7 @@ package edu.northeastern.cs4500;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpoiledTomatillosApplication {
 
