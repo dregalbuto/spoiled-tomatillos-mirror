fa26fd428fe4cb001819fe27ba9a5fe8ee0dc2be
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-02-13 10:29:54 -0500

    don't need to extend repository, the getbyName function can be made automatically

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/spoiledTomatillos/data/PrivilegeRepositoryCustom.java
+++
@@ -1,5 +0,0 @@
-package edu.northeastern.cs4500.spoiledTomatillos.data;
-
-public interface PrivilegeRepositoryCustom {
-	Privilege findByName(String name);
-}
--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/spoiledTomatillos/data/RoleRepositoryCustom.java
+++
@@ -1,5 +0,0 @@
-package edu.northeastern.cs4500.spoiledTomatillos.data;
-
-public interface RoleRepositoryCustom {
-	Role findByName(String name);
-}
