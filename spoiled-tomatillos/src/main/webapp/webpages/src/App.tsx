173cca2ee726a2e74133441b6c928c7e299472b1
erinzhangxi <erincheung1101@gmail.com>
2018-02-13 16:35:55 -0500

    revert changes made by a personal account

--- spoiled-tomatillos/src/main/webapp/webpages/src/App.tsx
+++ spoiled-tomatillos/src/main/webapp/webpages/src/App.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
 import './App.css';
-
-const logo = require('./logo.svg');
+import HomeScreen from './components/HomeScreen';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Spoiled Tomatillos</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          You can check out movies here!
         </p>
       </div>
     );
--- spoiled-tomatillos/src/main/webapp/webpages/src/index.tsx
+++ spoiled-tomatillos/src/main/webapp/webpages/src/index.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import Hello from './components/Hello';
 
-// TODO Change display hello back to app.tsx
-/*ReactDOM.render(
+// Change display hello back to app.tsx
+ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
-);*/
-ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
-  document.getElementById('root') as HTMLElement
 );
+
+// ReactDOM.render(
+//   <Hello name="TypeScript" enthusiasmLevel={10} />,
+//   document.getElementById('root') as HTMLElement
+// );
 
 registerServiceWorker();
