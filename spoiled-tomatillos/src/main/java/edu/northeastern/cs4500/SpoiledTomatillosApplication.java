21fb6d17dec0c7a33f589bcad593e1d96f827a8b
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-02-12 22:41:18 -0500

    setup and reorg

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/SpoiledTomatillosApplication.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/SpoiledTomatillosApplication.java
@@ -2,12 +2,19 @@ package edu.northeastern.cs4500;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+
+import edu.northeastern.cs4500.config.MvcConfig;
 
 
 @SpringBootApplication
 public class SpoiledTomatillosApplication {
 
+	private static TemplateEngine templateEngine;
+		
 	public static void main(String[] args) {
+		templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(new MvcConfig().templateResolver());
 		SpringApplication.run(SpoiledTomatillosApplication.class, args);
 	}
 }
