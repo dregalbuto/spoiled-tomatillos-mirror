1c749d4d54306b3d1faeb8c79724948b5dca5c39
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-02-12 22:59:13 -0500

    Revert "setup and reorg"
    This reverts commit 21fb6d17dec0c7a33f589bcad593e1d96f827a8b.

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/SpoiledTomatillosApplication.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/SpoiledTomatillosApplication.java
@@ -2,19 +2,12 @@ package edu.northeastern.cs4500;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thymeleaf.TemplateEngine;
-
-import edu.northeastern.cs4500.config.MvcConfig;
 
 
 @SpringBootApplication
 public class SpoiledTomatillosApplication {
 
-	private static TemplateEngine templateEngine;
-		
 	public static void main(String[] args) {
-		templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(new MvcConfig().templateResolver());
 		SpringApplication.run(SpoiledTomatillosApplication.class, args);
 	}
 }
