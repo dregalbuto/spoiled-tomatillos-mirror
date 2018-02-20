97fcdcbadd87bd3de2da6eeff34ab2d082c179e6
jiameng.wang <wang.jiame@husky.neu.edu>
2018-02-19 19:08:16 -0500

    search component template

---
+++ spoiled-tomatillos/src/main/webapp/script/src/components/Search.js
@@ -0,0 +1,13 @@
+import Search from 'react-search';
+import ReactDOM from 'react-dom';
+import React, {Component, PropTypes} from 'react';
+
+class Search extends Component {
+  {/* pass items = return by api request*/}
+
+  {/*render items*/}
+
+
+}
+
+export default Search;
