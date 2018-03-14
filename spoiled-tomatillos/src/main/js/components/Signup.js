5a089f4917ae42c7be52c12c63662637e9be13b8
xizhang2 <zhang.xi2@husky.neu.edu>
2018-03-13 20:17:59 -0400

    adjust spacing

--- spoiled-tomatillos/src/main/js/components/Signup.js
+++ spoiled-tomatillos/src/main/js/components/Signup.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './Login.css';
 var _ = require('underscore');
+import "./Signup.css";
+import {Form, Button, FormGroup, ControlLabel, FormControl, Col} from 'react-bootstrap'
 
 class Signup extends Component {
 	constructor(props) {
@@ -78,51 +79,109 @@ class Signup extends Component {
 				<div className="create_account_form">
 				<h1>Create account</h1>
 				<p>Create a user account in Spoiled Tomatillos</p>
-				<form onSubmit={this.onSubmit} >
 				
-				<input name = "email" placeholder = "Email" ref="email" type="text"
-					value={this.state.email} 
-					onChange={this.onChange} 
-				/>
+				<Form horizontal onSubmit={this.onSubmit}>
 				
+			
+				<FormGroup controlId="formHorizontalEmail">
+				 <Col componentClass={ControlLabel} sm={2}>
+			      Email
+			    </Col>
+			      <Col sm={10}>
+			      <input name = "email" placeholder = "Email" ref="email" type="text"
+						value={this.state.email} 
+						onChange={this.onChange} 
+					/>
+			    </Col>
+				</FormGroup>
+				
+				
+				 <FormGroup controlId="formHorizontalConfirmEmail">
+				 <Col componentClass={ControlLabel} sm={2}>
+			      Confirm Email
+			    </Col>
+			      <Col sm={10}>
 				<input name = "confirmemail" placeholder = "Confirm Email" ref="confirmemail" type="text"
 					value={this.state.confirmemail} 
 					onChange={this.onChange} 
 				/>
-
+				</Col>
+				</FormGroup>
+				
+				 <FormGroup controlId="formHorizontalFirstName">
+				 <Col componentClass={ControlLabel} sm={2}>
+			      First Name
+			    </Col>
+			      <Col sm={10}>
 				<input name = "first_name" placeholder = "First Name" ref="first_name" type="text"
 					value={this.state.first_name}
 					onChange={this.onChange} 
 				/> 
+				</Col>
+				</FormGroup>
 				
+				 <FormGroup controlId="formHorizontalLastName">
+				 <Col componentClass={ControlLabel} sm={2}>
+				 Last Name
+				 </Col>
+			      <Col sm={10}>
 				<input name = "last_name" placeholder = "Last Name" ref="last_name" type="text"
 					value={this.state.last_name}
 					onChange={this.onChange} 
 				/> 
+				</Col>
+				</FormGroup>
 				
+				
+				 <FormGroup controlId="formHorizontalUserName">
+				 <Col componentClass={ControlLabel} sm={2}>
+				 User Name
+				 </Col>
+			      <Col sm={10}>
 				<input name = "username" placeholder = "Username" ref="username"
 					value={this.state.username}
 					onChange={this.onChange} 
 				/> 
+				</Col>
+				</FormGroup>
 				
+				 <FormGroup controlId="formHorizontalPassword">
+				 <Col componentClass={ControlLabel} sm={2}>
+				 Password
+				 </Col>
+			      <Col sm={10}>
 				<input name= "password" placeholder = "Password" type="password"
 					ref="password" value={this.state.passsword}
 					onChange={this.onChange} 
 				/> 
-
+				</Col>
+				</FormGroup>
+				
+				 <FormGroup controlId="formHorizontalConfirmpassword">
+				 <Col componentClass={ControlLabel} sm={2}>
+				Confirm Password
+				 </Col>
+			      <Col sm={10}>
 				<input name = "passwordconfirm" ref="passwordconfirm" placeholder = "Confirm Password"
 					type="password" value={this.state.passwordconfirm}
 					onChange={this.onChange} 
 				/> 
-
-				<button type="submit" className="button button_wide">
+				</Col>
+				</FormGroup>
+				
+				<FormGroup>
+				 <Col smOffset={2} sm={10}>
+				<Button type="submit" className="button button_wide">
 				CREATE ACCOUNT
-				</button>
-
-				</form>
+				</Button>
+				</Col>
+				</FormGroup>
+				
+			
+				</Form>
 				</div>
 				</div>
-		);
+		)
 	}
 }
 
