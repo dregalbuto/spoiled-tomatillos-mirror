66b97aea6da44f50cd7d55c92841dca4938a824b
xizhang2 <zhang.xi2@husky.neu.edu>
2018-03-14 12:50:45 -0400

    TEAM24-99 #integrate signup and partial login

--- spoiled-tomatillos/src/main/js/components/Signup.js
+++ spoiled-tomatillos/src/main/js/components/Signup.js
@@ -60,6 +60,7 @@ class Signup extends Component {
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return re.test(event);
 	}
+
 	
 	onSubmit(e) {
 		e.preventDefault();
