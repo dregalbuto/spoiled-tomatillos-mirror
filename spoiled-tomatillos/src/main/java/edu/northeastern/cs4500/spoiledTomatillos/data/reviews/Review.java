04ffbe9eeb00025c3dddf00c02589536fdc5dd39
fionatran <tran.f@husky.neu.edu>
2018-03-15 00:29:34 -0400

    Creating Review Object

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/spoiledTomatillos/data/reviews/Review.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/spoiledTomatillos/data/reviews/Review.java
@@ -1,11 +1,12 @@
 package edu.northeastern.cs4500.spoiledTomatillos.data.reviews;
 
+import edu.northeastern.cs4500.spoiledTomatillos.data.movies.Movie;
+import edu.northeastern.cs4500.spoiledTomatillos.data.user.User;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.Collection;
+
 
 /**
  * Class for a review written by a user
@@ -14,16 +15,30 @@ import javax.persistence.ManyToOne;
 @Data
 @Entity(name="reviews")
 public class Review {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String text;
     private int rating;
 
+    /**
+     * A movie has many reviews.
+     */
     @ManyToOne
-    @JoinTable
+    @JoinColumn(name="movies")
+    private Collection<Movie> movie;
 
+    /**
+     * A user has many reviews.
+     */
+    @ManyToOne
+    @JoinColumn(name="users")
+    private Collection<User> user;
 
     public Review() {
 
     }
+
+
 }
