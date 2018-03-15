d7eab7bc944ea62b5ff611875782c674f5aa8ec7
jiameng.wang <wang.jiame@husky.neu.edu>
2018-03-15 00:58:16 -0400

    TEM24-117 add fb login to db

--- spoiled-tomatillos/src/main/js/components/Login.js
+++ spoiled-tomatillos/src/main/js/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Login.css';
-import FacebookLogin from 'react-facebook-login';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 
 const responseFacebook = (response) => {
   console.log(response);
@@ -10,70 +10,99 @@ class Login extends Component{
   constructor(props) {
     super(props);
     this.state={
-      username:"",
-      password:""
+      username:'',
+      password:'',
+      fb_username:'',
+      fb_firstname:'',
+      fb_lastname:'',
+      fb_email:'',
+      fb_password:''
     };
 
     this.handleClick = this.handleClick.bind(this);
+
   }
 
-  handleClick(event){
-    event.preventDefault;
+  responseFacebook(response){
+    console.log(response);
 
-    var apiBaseUrl = "http://localhost:8080/api/";
-    var self = this;
+    var newUser = {
+      first_name: response.name,
+      last_name: response.name,
+      email: "TEST@facebook.com",
+      username: response.name,
+      password: response.id
+    };
 
-    if(this.state.username.length <= 0 || this.state.password.length <= 0) {
-      alert("empty fields");
-      return;
-    }
 
-    post('http://localhost:8080/api/users',{
-      username:this.state.username,
-      password:this.state.password
-    }).then((res)=>{
-
+    fetch('http://localhost:8080/api/users',
+    {   method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser)
     })
-
-
+    .then(
+      console.log("POST success")
+    )
+    .catch( err => console.error(err))
   }
 
-  render() {
-    return(
-      <div className="Login">
-      <div className="container-fluid">
-      <h1>Login</h1>
-        <input
-        type="text"
-        floatingLabelText="username"
-        onChange = {(event, newValue) => this.setState({username:newValue})}
-        placeholder="username"/>
+handleClick(event){
+  event.preventDefault;
 
-        <input
-        type="password"
-        floatingLabelText="password"
-        onChange = {(event, newValue) => this.setState({password:newValue})}
-        placeholder="password"/>
+  var apiBaseUrl = "http://localhost:8080/api/";
+  var self = this;
 
-        <a className="btn btn-success btn-block" onClick={(event)=>this.handleClick(event)}
-        >Login</a>
-
-        <FacebookLogin
-          appId="1229282497194175"
-          autoLoad
-          callback={responseFacebook}
-          render={renderProps => (
-            <button onClick={renderProps.onClick}>This is my custom FB button</button>
-          )}
-        />
-
-        <a className="btn btn-success btn-block"
-        href="/Signup">Register</a>
-        </div>
-        </div>
-
-      );
-    }
+  if(this.state.username.length <= 0 || this.state.password.length <= 0) {
+    alert("empty fields");
+    return;
   }
 
-  export default Login;
+  get('http://localhost:8080/api/users',{
+    username:this.state.username,
+    password:this.state.password
+  }).then((res)=>{
+
+  })
+}
+
+render() {
+  return(
+    <div className="Login">
+    <div className="container-fluid">
+    <h1>Login</h1>
+    <input
+    type="text"
+    className="username"
+    onChange = {(event, newValue) => this.setState({username:newValue})}
+    placeholder="username"/>
+
+    <input
+    type="password"
+    className="password"
+    onChange = {(event, newValue) => this.setState({password:newValue})}
+    placeholder="password"/>
+
+    <a className="btn btn-success btn-block" onClick={(event)=>this.handleClick(event)}
+    >Login</a>
+
+    <FacebookLogin
+    appId="1229282497194175"
+    autoLoad
+    callback={this.responseFacebook}
+    render={renderProps => (
+      <button onClick={renderProps.onClick}>This is my custom FB button</button>
+    )}
+    />
+
+    <a className="btn btn-success btn-block"
+    href="/Signup">Register</a>
+    </div>
+    </div>
+
+  );
+}
+}
+
+export default Login;
