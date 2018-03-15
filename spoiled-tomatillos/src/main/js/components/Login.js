cc405ee19abf74782ba394f09a109eaf8e6d317b
YiwenDong <dong.yiw@husky.neu.edu>
2018-03-15 03:46:24 -0400

    Integrate facebook and normal login with redirect TEAM24-117

--- spoiled-tomatillos/src/main/js/components/Login.js
+++ spoiled-tomatillos/src/main/js/components/Login.js
@@ -24,84 +24,101 @@ class Login extends Component{
   }
 
   responseFacebook(response){
+    console.log("FACEBOOK RESPONSE");
     console.log(response);
 
     var newUser = {
       first_name: response.name,
       last_name: response.name,
-      email: "TEST@facebook.com",
+      email: response.name + "@facebook.com",
       username: response.name,
       password: response.id
     };
 
 
-    fetch('http://localhost:8080/api/users',
+    fetch('/api/users',
     {   method: 'POST',
       headers: {
       'Content-Type': 'application/json',
       },
       body: JSON.stringify(newUser)
     })
-    .then(
-      console.log("POST success")
+    .then( () => {
+        console.log("POST success");
+        window.location = "/Home";
+      }
     )
     .catch( err => console.error(err))
   }
 
-handleClick(event){
-  event.preventDefault;
-  var apiBaseUrl = "http://localhost:8080/api/";
-  var self = this;
+  handleClick(event){
+    event.preventDefault;
+    var apiBaseUrl = "/api/";
+    var name = this.username.value;
+    var pass = this.password.value;
 
-  if(this.state.username.length <= 0 || this.state.password.length <= 0) {
-    alert("empty fields");
-    return;
+    if(name.length <= 0 || pass.length <= 0) {
+      alert("empty fields");
+      return;
+    }
+
+    fetch('/api/users',{
+      username:name,
+      password:pass
+    }).then((res)=>{
+      return res.text();
+    }).then((tex) => {
+      var data = JSON.parse(tex);
+      var users = data["_embedded"].users;
+      for (var i = 0; i < users.length; i++) {
+        if (users[i].username == name) {
+          window.location = "/Home";
+        }
+      }
+      alert("User not found");
+    });
   }
 
-  get('http://localhost:8080/api/users',{
-    username:this.state.username,
-    password:this.state.password
-  }).then((res)=>{
+  render() {
+    return(
+      <div className="Login">
+      <div className="container-fluid">
+      <h1>Login</h1>
+      <input
+      type="text"
+      className="username"
+      ref= {(input) => {this.username = input;}}
+      onChange = {(event, newValue) => this.setState((prev, props) =>
+      {prev.username = newValue; return prev;})}
+      placeholder="username"/>
 
-  })
-}
+      <input
+      type="password"
+      className="password"
+      ref= {(input) => {this.password = input;}}
+      onChange = {(event, newValue) => this.setState((prev, props) =>
+      {prev.password = newValue; console.log(newValue); return prev;})}
+      placeholder="password"/>
 
-render() {
-  return(
-    <div className="Login">
-    <div className="container-fluid">
-    <h1>Login</h1>
-    <input
-    type="text"
-    className="username"
-    onChange = {(event, newValue) => this.setState({username:newValue})}
-    placeholder="username"/>
+      <a className="btn btn-success btn-block" onClick={(event)=>this.handleClick(event)}
+      >Login</a>
 
-    <input
-    type="password"
-    className="password"
-    onChange = {(event, newValue) => this.setState({password:newValue})}
-    placeholder="password"/>
+      <FacebookLogin
+      appId="1229282497194175"
+      autoLoad
+      callback={this.responseFacebook}
+      render={renderProps => (
+        <button onClick={renderProps.onClick}>Login with facebook</button>
+      )}
+      />
 
-    <a className="btn btn-success btn-block" onClick={(event)=>this.handleClick(event)}
-    >Login</a>
-
-    <FacebookLogin
-    appId="1229282497194175"
-    autoLoad
-    callback={this.responseFacebook}
-    render={renderProps => (
-      <button onClick={renderProps.onClick}>This is my custom FB button</button>
-    )}
-    />
-
-    <a className="btn btn-success btn-block"
-    href="/Signup">Register</a>
+      <a className="btn btn-success btn-block"
+      href="/Signup">Register</a>
     </div>
     </div>
 
-  );
-}
+    );
+  }
 }
 
 export default Login;
