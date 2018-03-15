d78bf14f7ac7a69f526c0bcc044d036af5517566
xizhang2 <zhang.xi2@husky.neu.edu>
2018-03-15 17:34:55 -0400

    TEAM24-98 #homepage navigation bar, log in page styling

--- spoiled-tomatillos/src/main/js/components/Homepage.js
+++ spoiled-tomatillos/src/main/js/components/Homepage.js
@@ -7,113 +7,128 @@ import {Navbar, NavItem, NavDropdown, MenuItem, Nav} from 'react-bootstrap';
 
 class Movies extends Component {
 
-  render() {
-    return(
-        <div className="Title">
-          <h2>Explore Movies</h2>
-        </div>
-    );
-  }
+	render() {
+		return(
+				<div className="Title">
+				<h2>Explore Movies</h2>
+				</div>
+		);
+	}
 }
 
 class Celebs extends Component {
-  render() {
-    return(
-        <div className="Title">
-          <h2>Explore Celebrities</h2>
-        </div>
-    );
-  }
+	render() {
+		return(
+				<div className="Title">
+				<h2>Explore Celebrities</h2>
+				</div>
+		);
+	}
 }
 class Reviews extends Component {
-  render() {
-    return(
-        <div className="Title">
-          <h2>Explore Reviews</h2>
-        </div>
-    );
-  }
+	render() {
+		return(
+				<div className="Title">
+				<h2>Explore Reviews</h2>
+				</div>
+		);
+	}
 }
 
 const helloAPI = 'http://www.omdbapi.com/?i=tt3896198&apikey=1c821225' ;
 console.log("Hello from omdb");
 
 class Homepage extends Component {
-  constructor() {
-    super();
-    this.name = "Spoiled Tomatillos";
-    this.state = {
-      data:[],
-    }}
+	constructor() {
+		super();
+		this.name = "Spoiled Tomatillos";
+		this.state = {
+				data:[],
+		}}
 
-    componentDidMount() {
-      fetch('http://www.omdbapi.com/?i=tt3896198&apikey=1c821225')
-      .then((response) => response.json())
-      .then((findresponse) => {
-        console.log(findresponse.Poster)
-        this.setState({
-          data:findresponse.Poster,
-        })
-      })
-    }
-  render() {
-    return (
-    		<div className="Homepage">
-      <Navbar>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#home">NONONONO</a>
-    </Navbar.Brand>
-  </Navbar.Header>
-  <Nav>
-    <NavItem eventKey={1} href="#">
-      Link
-    </NavItem>
-    <NavItem eventKey={2} href="#">
-      Link
-    </NavItem>
-    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-      <MenuItem eventKey={3.1}>Action</MenuItem>
-      <MenuItem eventKey={3.2}>Another action</MenuItem>
-      <MenuItem eventKey={3.3}>Something else here</MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey={3.4}>Separated link</MenuItem>
-    </NavDropdown>
-  </Nav>
-</Navbar>
+	componentDidMount() {
+		fetch('http://www.omdbapi.com/?i=tt3896198&apikey=1c821225')
+		.then((response) => response.json())
+		.then((findresponse) => {
+			console.log(findresponse.Poster)
+			this.setState({
+				data:findresponse.Poster,
+			})
+		})
+	}
+	render() {
+		return (
+				<div className="Homepage">
+				
+				<nav id="topNav" className="navbar navbar-fixed-top">
+				<div className="container-fluid">
+				
+				<Navbar inverse collapseOnSelect>
+				  <Navbar.Header>
+				    <Navbar.Brand>
+				      <a href="#brand">Spoiled Tomatillos</a>
+				    </Navbar.Brand>
+				    <Navbar.Toggle />
+				  </Navbar.Header>
+				  <Navbar.Collapse>
+				    <Nav>
+				      <NavItem eventKey={1} href="#">
+				        Movies
+				      </NavItem>
+				      <NavItem eventKey={2} href="#">
+				        Reviews
+				      </NavItem>
+				        <NavItem eventKey={3} href="#">
+				        News
+				      </NavItem>
+				      <NavDropdown eventKey={3} title="Categories" id="basic-nav-dropdown">
+				        <MenuItem eventKey={3.1}>Comedy</MenuItem>
+				        <MenuItem eventKey={3.2}>Sci-Fi</MenuItem>
+				        <MenuItem eventKey={3.3}>Horror</MenuItem>
+				        <MenuItem eventKey={3.4}>Romance</MenuItem>
+				        <MenuItem eventKey={3.5}>Action</MenuItem>
+				        <MenuItem eventKey={3.6}>Drama</MenuItem>
+		
+				        <MenuItem divider />
+				        <MenuItem eventKey={3.7}>More</MenuItem>
+				      </NavDropdown>
+				    </Nav>
+				    <Nav pullRight>
+				    <NavItem>
+				    <li><Link to="/Signup"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
+				    </NavItem>
+				    <NavItem>
+				    <li><Link to="/Login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
+				    </NavItem>
+				    </Nav>
+				  </Navbar.Collapse>
+				</Navbar>
+				
+				</div>
+				</nav>
 
 
+				<header className="header">
 
-<header className="header">
+				<h1 className="App-title">Welcome to {this.name}</h1>
+				<p className="App-intro">
+				Find Movies, TV shows, Celebrities and more ...
+				</p>
+				</header>
+				<SearchBar />
 
-      <nav id="topNav" className="navbar navbar-fixed-top">
-      <div className="container-fluid">
-      <ul className="nav navbar-nav navbar-right">
-      <li><Link to="/Signup"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
-      <li><Link to="/Login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
-      </ul>
-      </div>
-      </nav>
+				<div className = "movie-items">
+				{
+					<img src = {this.state.data} alt="movie-item"></img>
+				}
+				</div>
+				<Movies />
+				<Celebs />
+				<Reviews/>
 
-      <h1 className="App-title">Welcome to {this.name}</h1>
-      <p className="App-intro">
-      Find Movies, TV shows, Celebrities and more ...
-      </p>
-      </header>
-      <SearchBar />
-
-      <div className = "movie-items">
-      {
-        <img src = {this.state.data} alt="movie-item"></img>
-      }
-      </div>
-      <Movies />
-      <Celebs />
-      <Reviews/>
-
-      </div>
-    );
-  }
+				</div>
+		);
+	}
 }
 
 export default Homepage;
