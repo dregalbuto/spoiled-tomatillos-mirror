12025112fe9f43e687b94071f1ce50f3da7475fc
Diana Regalbuto <regalbuto.d@husky.neu.edu>
2018-03-14 20:53:48 -0400

    manually add getters and setters for user

--- spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/spoiledTomatillos/user/model/User.java
+++ spoiled-tomatillos/src/main/java/edu/northeastern/cs4500/spoiledTomatillos/user/model/User.java
@@ -20,7 +20,7 @@ import lombok.Data;
 /**
  * Class for a user of Spoiled Tomatillos
  */
-@Data
+
 @Entity(name="users")
 @CrossOrigin(origins = "http://localhost:3000")
 public class User {
@@ -33,6 +33,43 @@ public class User {
 	private String email;
 	private String username;
 	private String password;
+	
+	public void setFirstName(String firstName) {
+		this.first_name = firstName;
+	}
+	
+	public String getFirstName() {
+		return first_name;
+	}
+	
+	public void setLastName(String lastName) {
+		this.last_name = lastName;
+	}
+	
+	public String getLastName() {
+		return last_name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
 
 	public void setPassword(String password) {
 		this.password = BCrypt.hashpw(password, BCrypt.gensalt());
