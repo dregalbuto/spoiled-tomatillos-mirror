e032de3c28583b712829a06ad8d1500035d77cdf
xizhang2 <zhang.xi2@husky.neu.edu>
2018-03-21 14:16:29 -0400

    TEAM24-125 #add comment block

--- spoiled-tomatillos/src/main/js/components/Movie.js
+++ spoiled-tomatillos/src/main/js/components/Movie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Movie.css';
 import MovieCard from './MovieCard.js';
 import { Rating } from 'semantic-ui-react';
+import { Button, Comment, Form, Header } from 'semantic-ui-react'
 
 
 /**
@@ -41,7 +42,7 @@ class Movie extends Component {
         });
   }
   handleChange(e) {
-	  this.setState({ this.rating: e.target.value })
+	  this.setState({ rating: e.target.value })
   }
 
   /**
@@ -58,13 +59,95 @@ class Movie extends Component {
     } else {
       return (
         <div>
-          <MovieCard data={this.state}/>
-          <br />
+        	  <div>
+            <MovieCard data={this.state}/>
+          </div>
           <div>Rating: {rating}</div>
           <br />
           <div>
           		<Rating icon='star' size='massive' defaultRating={3} maxRating={5} />
           </div>
+          		
+         
+          		
+          		
+          <Comment.Group>	
+            <Header as='h3' dividing>Comments</Header>
+          
+
+            <Comment>
+              <Comment.Avatar src="https://materiell.com/wp-content/uploads/2015/03/rebecca-full.png" />
+              <Comment.Content>
+                <Comment.Author as='a'>Veronica</Comment.Author>
+                <Comment.Metadata>
+                  <div>Today at 5:42PM</div>
+                </Comment.Metadata>
+                <Comment.Text>How artistic!</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+
+            
+            <Comment>
+            <Comment.Avatar src='https://materiell.com/wp-content/uploads/2015/03/mikec_full.png' />
+                <Comment.Content>
+                  <Comment.Author as='a'>Elliot Fu</Comment.Author>
+                  <Comment.Metadata>
+                    <div>Yesterday at 12:30AM</div>
+                  </Comment.Metadata>
+                  <Comment.Text>
+                    <p>I'm very inspired by this movie!! Thanks as well!</p>
+                  </Comment.Text>
+                  <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                  </Comment.Actions>
+                </Comment.Content>
+                
+                <Comment.Group>
+                <Comment>
+                  <Comment.Avatar src='https://materiell.com/wp-content/uploads/2015/03/liz_full1.png' />
+                  <Comment.Content>
+                    <Comment.Author as='a'>Jenny Hess</Comment.Author>
+                    <Comment.Metadata>
+                      <div>Just now</div>
+                    </Comment.Metadata>
+                    <Comment.Text>
+                      Elliot you are always so right :)
+                    </Comment.Text>
+                    <Comment.Actions>
+                      <Comment.Action>Reply</Comment.Action>
+                    </Comment.Actions>
+                  </Comment.Content>
+                </Comment>
+              </Comment.Group>
+            </Comment>
+            
+            
+            <Comment>
+            <Comment.Avatar src='https://materiell.com/wp-content/uploads/2015/03/bill-small.png' />
+            <Comment.Content>
+              <Comment.Author as='a'>Joe Henderson</Comment.Author>
+              <Comment.Metadata>
+                <div>5 days ago</div>
+              </Comment.Metadata>
+              <Comment.Text>
+                Dude, this is awesome. Thanks so much
+              </Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+
+          
+          <Form reply>
+            <Form.TextArea />
+            <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+          </Form>
+          
+          </Comment.Group>
           </div>
       )
     }
