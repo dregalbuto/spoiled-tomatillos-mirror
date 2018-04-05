cd5e9072799ba0e41642c84c9dc4ea8a2bcc502b
Yiwen Dong <dong.yiw@husky.neu.edu>
2018-04-05 11:29:25 -0400

    Improved rendering

--- spoiled-tomatillos/src/main/js/components/MovieCard.js
+++ spoiled-tomatillos/src/main/js/components/MovieCard.js
@@ -76,50 +76,74 @@ class MovieCard extends Component {
    * Render this as defined with the data from the structure as shown above as this.state.
    */
   render() {
-    let data = this.state;
+      let data = this.state;
 
-    let posterIMG = data.poster,
-        production = data.production,
-        productionCountries = data.production_countries,
-        genres = data.genre,
-        noData = '-';
+      let posterIMG = data.poster,
+          production = data.production,
+          productionCountries = data.production_countries,
+          genres = data.genre,
+          noData = '-';
 
-    // conditional statements for no data
-    if (data.vote === undefined || data.vote === 0) {
-      data.vote = noData;
-    } else {
-      data.vote = data.vote + ' / 10';
-    }
+      // conditional statements for no data
+      if (data.vote === undefined || data.vote === 0) {
+        data.vote = noData;
+      } else {
+        data.vote = data.vote + ' / 10';
+      }
 
-    if(data.poster== null){
-        posterIMG = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSols5HZxlQWyS9JY5d3_L9imbk0LiziHiyDtMZLHt_UNzoYUXs2g';
-    }
+      if(data.poster== null){
+          posterIMG = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSols5HZxlQWyS9JY5d3_L9imbk0LiziHiyDtMZLHt_UNzoYUXs2g';
+      }
 
-    return (
-      <div className="col-xs-12 cardcont nopadding">
+      if(data.size == "big") {
+        return (
+          <div className="col-xs-12 cardcont nopadding">
 
-         <div className="meta-data-container col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5">
-           <h1>{data.original_title}</h1>
+           <div className="meta-data-container col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5">
+             <h1>{data.original_title}</h1>
 
-           <span className="tagline">{data.tagline}</span>
-           <p>{data.overview}</p>
-           <div className="additional-details">
-             <div className="row nopadding release-details">
-               <div className="col-xs-6"> Original Release: <span className="meta-data">{data.release}</span></div>
-               <div className="col-xs-6"> Running Time: <span className="meta-data">{data.runtime}</span> </div>
-               <div className="col-xs-6"> Vote Average: <span className="meta-data">{data.vote}</span></div>
+             <span className="tagline">{data.tagline}</span>
+             <p>{data.overview}</p>
+             <div className="additional-details">
+               <div className="row nopadding release-details">
+                 <div className="col-xs-6"> Original Release: <span className="meta-data">{data.release}</span></div>
+                 <div className="col-xs-6"> Running Time: <span className="meta-data">{data.runtime}</span> </div>
+                 <div className="col-xs-6"> Vote Average: <span className="meta-data">{data.vote}</span></div>
+               </div>
              </div>
-           </div>
            <div>
+             <Link to={"www.fandango.com/search?q=" + data.original_title + "&mode=Movies"}>Fandango</Link>
+             <Link to={"www.imdb.com/title/" + data.movieID}>IMDB</Link>
+           </div>
+           <div className="poster-container nopadding col-xs-12 col-md-4 pull-md-8 col-lg-5 pull-lg-7 ">
+             <img id="postertest" className='poster' src={posterIMG}/>
            </div>
          </div>
-         <div className="poster-container nopadding col-xs-12 col-md-4 pull-md-8 col-lg-5 pull-lg-7 ">
-           <img id="postertest" className='poster' src={posterIMG}/>
-         </div>
-       </div>
-    );
+        );
+      }
+      if (data.size == "normal") {
+        return (
+              <div className="col-xs-12 cardcont nopadding">
 
-  }
+               <div className="meta-data-container col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5">
+                 <h1>{data.original_title}</h1>
+                 <span className="tagline">{data.tagline}</span>
+                 <div className="poster-container nopadding">
+                   <img id="postertest" className='poster' src={posterIMG}/>
+                 </div>
+               <div>
+                 <Link to={"www.fandango.com/search?q=" + data.original_title + "&mode=Movies"}>Fandango</Link>
+                 <Link to={"www.imdb.com/title/" + data.movieID}>IMDB</Link>
+               </div>
+             </div>
+        );
+      }
+      return (
+                 <div className="poster-container nopadding">
+                   <img id="postertest" className='poster' src={posterIMG}/>
+                 </div>
+      );
+    }
 }
 
 export default MovieCard;
