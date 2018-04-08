f3aa2a2a42c4bcfb9a02ed11e5ce35c35f35605b
xizhang2 <zhang.xi2@husky.neu.edu>
2018-04-08 14:08:05 -0400

    TEAM24-165 #able to fetch requests data, but need a component to convert user ID to object

--- spoiled-tomatillos/src/main/js/components/FriendList.js
+++ spoiled-tomatillos/src/main/js/components/FriendList.js
@@ -3,85 +3,86 @@ import { Button, Image, List, Container, Divider,
 Grid, Header,Icon, Menu, Segment, Item, Label, Table, Statistic, Pagination} from 'semantic-ui-react'
 import NavigationBar from './NavigationBar.js';
 import { Link } from 'react-router-dom';
+import cookie from 'react-cookies'
 
-
-class UserHeading extends Component {
+class Request extends Component {
   constructor() {
     super();
     this.state={};
   }
-  render() {
+  render(){
     return (
-      <Container text>
-        <Header
-          as='h2'
-          content='username'
-          inverted
-          style={{
-            fontSize: '1.7em',
-            fontWeight: 'normal',
-            marginTop: '1.5em',
-          }}
-        >
-        <Grid>
-        <Grid.Column width={4}>
-        <Image circular src='https://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-        {' '}Patrick
-        </Grid.Column>
-        <Grid.Column width={9}>
+      <Table.Row>
+      <Table.Cell><div>{this.state.request.name}</div></Table.Cell>
+      <Table.Cell><div>{this.state.request.creator.id}</div></Table.Cell>
+      <Table.Cell><div>{this.state.request.topic.id}</div></Table.Cell>
+      <Table.Cell>
+      <ManageGroup />
+      <Modal
+      open={open} basic size='small'
+        onOpen={this.open}
+      onClose={this.close}
+      style={{height: 300}}
+      trigger={<Button icon><Icon link name='delete' /></Button>}>
 
-        <Segment inverted>
-        <Statistic.Group inverted>
-          <Statistic color='blue' size='small'>
-            <Statistic.Value>39</Statistic.Value>
-            <Statistic.Label>Movies Watched</Statistic.Label>
-          </Statistic>
-          <Statistic color='blue' size='small'>
-            <Statistic.Value>562</Statistic.Value>
-            <Statistic.Label>Friends</Statistic.Label>
-          </Statistic>
-          <Statistic color='blue' size='small'>
-            <Statistic.Value>7</Statistic.Value>
-            <Statistic.Label>Groups</Statistic.Label>
-          </Statistic>
-        </Statistic.Group>
-        </Segment>
+      <Header icon='archive' content='Are you sure to delete this group?' />
+        <Modal.Content>
+      <p>Once you confirm, you will lose the members and reviews in this request.</p>
+      </Modal.Content>
+      <Modal.Actions>
+      <Button basic color='red' inverted onClick={this.close}>
+      <Icon name='remove' /> No
+      </Button>
 
+      <Button color='green' inverted onClick={this.close}>
+      <Icon name='checkmark' /> Yes
+      </Button>
+      </Modal.Actions>
+      </Modal>
 
-        </Grid.Column>
-        <Grid.Column width={3}>
-
-
-        <Header as='h3' icon inverted>
-        <Icon name='settings' />
-        Account Settings
-
-      </Header>
-
-        </Grid.Column>
-      </Grid>
-
-
-    </Header>
-
-       </Container>
-
+      </Table.Cell>
+      </Table.Row>
     )
   }
 }
-class FriendList extends Component {
+class FriendRequests extends Component {
   constructor() {
     super();
     this.state={};
   }
+  render() {
+    const requests = this.state.requests.map((request) =>
+			<Request key={request.id} request={request} />
+		);
 
+    return (
+      <div>
+      <h3>Friend Requests</h3>
+      <Table color='grey' key='grey'>
+      <Table.Header>
+      <Table.Row>
+      <Table.HeaderCell>User</Table.HeaderCell>
+      <Table.HeaderCell>Actions</Table.HeaderCell>
+      </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+      {requests}
+      </Table.Body>
+      </Table>
+      </div>
+    )
+  }
+}
+
+class ConnectedFriends extends Component {
+  constructor() {
+    super();
+    this.state={};
+  }
   render() {
     return (
       <div>
-      <NavigationBar />
-      <Link to="/user/:id"><Button basic inverted color='blue'>Back</Button></Link>
-      <UserHeading />
-
       <h3>Friend List</h3>
       <List divided verticalAlign='middle' size='massive'>
       <List.Item>
@@ -125,8 +126,110 @@ class FriendList extends Component {
       </div>
     )
   }
+}
 
+class UserHeading extends Component {
+  constructor() {
+    super();
+    this.state={};
+  }
+  render() {
+    return (
+      <Container text>
+        <Header
+          as='h2'
+          content='username'
+          inverted
+          style={{
+            fontSize: '1.7em',
+            fontWeight: 'normal',
+            marginTop: '1.5em',
+          }}
+        >
+        <Grid>
+        <Grid.Column width={4}>
+        <Image circular src='https://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
+        </Grid.Column>
+        <Grid.Column width={9}>
 
+        <Segment inverted>
+        <Statistic.Group inverted>
+          <Statistic color='blue' size='small'>
+            <Statistic.Value>39</Statistic.Value>
+            <Statistic.Label>Movies Watched</Statistic.Label>
+          </Statistic>
+          <Statistic color='blue' size='small'>
+            <Statistic.Value>562</Statistic.Value>
+            <Statistic.Label>Friends</Statistic.Label>
+          </Statistic>
+          <Statistic color='blue' size='small'>
+            <Statistic.Value>7</Statistic.Value>
+            <Statistic.Label>Groups</Statistic.Label>
+          </Statistic>
+        </Statistic.Group>
+        </Segment>
+        </Grid.Column>
+        <Grid.Column width={3}>
+        <Header as='h3' icon inverted>
+        <Icon name='settings' />
+        Account Settings
+      </Header>
+        </Grid.Column>
+      </Grid>
+    </Header>
+
+       </Container>
+
+    )
+  }
+}
+class FriendList extends Component {
+  constructor() {
+    super();
+    this.state={
+      cookies: cookie.load('user'),
+      requests: []
+    };
+    console.log("FRIENDLIST  cookies");
+    console.log(this.state.cookies);
+
+    var usertoken = this.state.cookies.user_token;
+    var useremail = this.state.cookies.email;
+
+    var data = {
+      "email":useremail,
+      "token":usertoken
+    }
+
+    var body = JSON.stringify(data);
+
+    fetch('/api/friend/requests', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: body
+    }).then(res=>res.json())
+      .then((res)=>{
+          console.log(res);
+  				this.setState({ requests: res })
+          console.log("FRIENDLIST  fetch requests");
+  				console.log(this.state.requests);
+      });
+
+  }
+  render() {
+    return (
+      <div>
+      <NavigationBar />
+      <Link to="/user/:id"><Button basic inverted color='blue'>Back</Button></Link>
+      <UserHeading />
+      <ConnectedFriends />
+      <FriendRequests requests={this.state.requests}/>
+      </div>
+    )
+  }
 }
 
 const style = {
